0a0672fa4e1666c37d02023f658f7f283195337b
wkaspryk <wkaspryk@gmail.com>
2016-11-20 17:37:23 -0800

    updates and cleanup

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 window.onload = function(){
-	var fadeInSection = document.getElementById('header');
-	var bottom_of_object, plx, bottom_of_window;
-
-	if(document.getElementById('header') !=undefined){
-		plx = document.getElementById('header');
-	}
 
 		window.onscroll = function(){
 
@@ -19,20 +13,5 @@ window.onload = function(){
 				document.getElementById('header').style.opacity='1';
 			}
 		}
-
-			//fade in content effect
-			if (fadeInSection!=undefined) {
-				
-				//bottom_of_object = contentList[i].getBoundingClientRect().bottom + ;
-				bottom_of_object = fadeInSection.getBoundingClientRect().top;
-				bottom_of_window = window.scrollY;
-
-				if(bottom_of_window > bottom_of_object && fadeInSection.className.indexOf('fade-in-active') == -1){
-
-					fadeInSection.className += " fade-in-active";					
-
-				}
-
-			}
 	}	
 }
