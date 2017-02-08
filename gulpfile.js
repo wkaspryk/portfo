14dcaa4fdfa25cea16129e3154bb4ec8df2568a5
wkaspryk <wkaspryk@gmail.com>
2017-02-07 22:03:18 -0800

    major edits, opening jquery

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+// var browserify = require('gulp-browserify');
+var sass = require('gulp-sass');
+
+/*
+ * Directories here
+ */
+
+var paths = {
+  sass: './src/sass/',
+  css: './css/',
+  data: './src/_data/'
+};
+
+/**
+ * Compile .scss files into public css directory With autoprefixer no
+ * need for vendor prefixes then live reload the browser.
+ */
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
