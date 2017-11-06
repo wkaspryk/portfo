ba9aa34d6389498edb9145c04bdf98b2fc237881
wkaspryk <wkaspryk@gmail.com>
2017-11-05 19:11:39 -0800

    updates fall 2017

--- script.js
+++ script.js
@@ -27,9 +27,7 @@ window.onload = function(){
 				var hash = this.hash;
 				$('html, body').animate({
 					scrollTop: $(hash).offset().top - 50
-				}, 800, function(){
-					window.location.hash = hash;
-				});
+				}, 800);
 			}
 		});
 	});
